0cdb8264ebaca20c5157dd285b9e7d9cc0589c8a
Viktor Likin <viktor.likin@gmail.com>
2015-04-27 10:00:30 +0300

    Slide separator.

--- drupal-7.36/sites/default/modules/bxslider/js/behaviors.js
+++ drupal-7.36/sites/default/modules/bxslider/js/behaviors.js
@@ -7,6 +7,13 @@
         var dom = $(this);
         var ukey = dom.attr('data-ukey');
         var settings = Drupal.settings.bxslider.formatter[ukey];
+
+        // Postprocess settings variables.
+        var property_list = ['slideWidth', 'slideMargin']
+        for (var index in property_list) {
+          var key = property_list[index];
+          settings[key] = parseInt(settings[key]);
+        }
         dom.bxSlider(settings);
       });
     }
