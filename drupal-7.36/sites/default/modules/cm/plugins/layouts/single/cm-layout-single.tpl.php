01a494440f0b4b6880bd03a7810623e6a5280f32
Viktor Likin <viktor.likin@gmail.com>
2015-04-27 07:23:21 +0300

    The single column - simple layout has been added.

---
+++ drupal-7.36/sites/default/modules/cm/plugins/layouts/single/cm-layout-single.tpl.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file Single column.
+ * 
+ * It contains the simple wrapper.
+ *
+ * Variables:
+ *  - content
+ */
+?>
+<table id="cm_layout_single">
+  <tr>
+    <td colspan="8"><?= $content['content'] ?></td>
+  </tr>
+</table>
---
+++ drupal-7.36/sites/default/themes/ct/templates/cm-layout-single.tpl.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @file It overrides a base template
+ */
+?>
+<div id="<?= $css_id ?>" class="<?= $classes ?>">
+  <?= $content['content'] ?>
+</div>
