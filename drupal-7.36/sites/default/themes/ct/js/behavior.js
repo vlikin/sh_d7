5c61b4b2d67ed17f050624880973e1ae78cc1b4d
Viktor Likin <viktor.likin@gmail.com>
2015-04-30 22:14:37 +0300

    Mobile menu.

--- drupal-7.36/sites/default/themes/ct/js/behavior.js
+++ drupal-7.36/sites/default/themes/ct/js/behavior.js
@@ -1,2 +1,13 @@
-jQuery(document).ready(function() {
-});
+(function ($) {
+  Drupal.behaviors.imce_element = {
+    attach: function (context, settings) {
+      $('.language-switcher-locale-url, .cm_cct_menu', context).click(function(event) {
+        $(this).toggleClass('show');
+      });
+      $('.language-switcher-locale-url li.active a, .cm_cct_menu a.active', context).click(function(event) {
+        event.preventDefault();
+      });
+
+    }
+  };
+}) (jQuery);
