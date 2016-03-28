221a0621941232219895bd59c427467d638e62ac
Viktor Likin <viktor.likin@gmail.com>
2016-03-28 20:54:05 +0300

    The loading popup.

--- drupal-7.36/sites/default/themes/ct/templates/html.tpl.php
+++ drupal-7.36/sites/default/themes/ct/templates/html.tpl.php
@@ -5,8 +5,31 @@
 <title><?php print $head_title;?></title>
 </head>
 <body>
-  <div id="ct-body-lock-shadow"></div>
-  <img id="ct-body-lock" src="<?= path_to_theme()  ?>/images/hourglass.svg" />
+  <div
+    id="ct-body-lock-shadow"
+    style="
+      background-color: #111;
+      opacity: 0.65;
+      *background: none;
+      position: fixed;
+      z-index: 9001;
+      top: 0px;
+      left: 0px;
+      width: 100%;
+      height: 100vh;
+  "></div>
+  <img
+    id="ct-body-lock"
+    src="<?= path_to_theme()  ?>/images/hourglass.svg"
+    style="
+      position: absolute;
+      z-index: 9002;
+      top: 50vh;
+      left: 50%;
+      width: 120px;
+      height: 120px;
+    "
+  />
   <?php print $page_top;?>
   <?php print $page;?>
   <?php print $page_bottom;?>
