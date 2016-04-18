58c24fa1e807a3cff9594b41a37ee8e48b803f37
Viktor Likin <viktor.likin@gmail.com>
2016-04-18 14:35:13 +0300

    The main template has been fixed. It shows metatags in the header.

--- drupal-7.36/sites/default/themes/ct/templates/html.tpl.php
+++ drupal-7.36/sites/default/themes/ct/templates/html.tpl.php
@@ -1,8 +1,9 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//RU" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title><?php print $head_title;?></title>
+  <head profile="<?= $grddl_profile ?>">
+    <?= $head ?>
+    <title><?= $head_title ?></title>
+  </head>
 </head>
 <body>
   <div
@@ -31,9 +32,9 @@
     ">
     <?php include path_to_theme() . "/images/hourglass.svg" ?>
 </div>
-  <?php print $page_top;?>
-  <?php print $page;?>
-  <?php print $page_bottom;?>
+  <?= $page_top ?>
+  <?= $page ?>
+  <?= $page_bottom ?>
 </body>
 <?php print $styles;?>
 <?php print $scripts;?>
